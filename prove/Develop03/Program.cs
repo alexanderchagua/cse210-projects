d4c6e7a1709fc9cae38bc054797e9b3683927bfa
alexandelton <alexanderelton2887@gmail.com>
2023-02-12 01:14:45 -0500

    newss

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,19 +1,47 @@
+
 using System;
 class Program
 {
     static void Main(string[] args){
-        Scripture scr = new Scripture("moroni 10:20","Wherefore, there must be afaith; and if there must be faith there must also be hope; and if there must be hope there must also be charity.");
-        Console.WriteLine(scr.Getscriture());
+        Console.Clear();
+        Random ran = new Random();
+        string scripture = "Wherefore, there must be afaith; and if there must be faith there must also be hope; and if there must be hope there must also be charity.";
 
 
-        Rndscripture myrdn = new Rndscripture();
-        myrdn._name = "BOOK IS:";
+        string[] scrip = scripture.Split(",");
+        List<string> asd = new List<string>(scrip);
+        List<int> indexTracker = new List<int>();
+        string value = "";
+        int rn = 0;
+        int indexUse = 24;
+        int i = 0;
 
-        myrdn._scriture.Add(scr);
+        do{
+            Console.Clear();
+            Console.WriteLine("moroni 10:20"+ string.Join("",asd));
+            Console.WriteLine("\n PRESS ENTER TO CONTINUE OR TYPE 'QUIT TO FINSH");
 
-        myrdn.Display();
-
-    }
+            value = Console.ReadLine();
+            if( value == "" || value == null ){
+                while(i<2){
 
     
-    }
+                {
+                string randonport = asd[ran.Next(0,asd.Count())];
+                i++;};
+
+                rn = ran.Next(0, asd.Count());
+
+                int aux = asd[rn].Length;
+                string str = new string('_', aux);
+                asd[rn] = str;
+
+                indexUse = rn;
+                indexTracker.Add(indexUse);
+
+            }
+        }
+    }while(value != "quit");
+    
+    
+    }}
